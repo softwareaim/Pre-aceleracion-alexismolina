92b65e7bb9dd590505e0466afebc5ab6d3454d64
Alexis <alexis_molina789@hotmail.com>
2022-04-12 02:22:24 -0300

    se logro setear id personaje y genero desde pelicula

---
+++ src/main/java/com/alkemy/peliculas/controller/PeliculaController.java
@@ -0,0 +1,44 @@
+package com.alkemy.peliculas.controller;
+
+import com.alkemy.peliculas.dto.PeliculaDTO;
+import com.alkemy.peliculas.service.impl.PeliculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class PeliculaController {
+
+    @Autowired
+    private PeliculaService peliculaService;
+
+    @GetMapping
+    public ResponseEntity<List<PeliculaDTO>> getAll() {
+        List<PeliculaDTO> dtos = this.peliculaService.getAll();
+        return ResponseEntity.ok().body(dtos);
+    }
+
+    @PostMapping
+    public ResponseEntity<PeliculaDTO> save(@RequestBody PeliculaDTO dto) {
+        PeliculaDTO result = this.peliculaService.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PeliculaDTO> update(@PathVariable Long id, @RequestBody PeliculaDTO dto) {
+        PeliculaDTO result = this.peliculaService.update(id, dto);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PeliculaDTO> delete(@PathVariable Long id) {
+        this.peliculaService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+}
